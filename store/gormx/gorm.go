a584e8bd371625741dfd0a0696fc858096c797dd
songhao <emberfarkas@hotmail.com>
2023-01-17 13:51:03 +0800

    fix: dbtype

--- store/gormx/gorm.go
+++ store/gormx/gorm.go
@@ -10,17 +10,6 @@ import (
 	gormlog "gorm.io/gorm/logger"
 )
 
-// DBType database type
-type DBType string
-
-const (
-	// dbMySQL Gorm Drivers mysql || postgres || sqlite || sqlserver
-	dbMySQL     DBType = "mysql"
-	dbPostgres  DBType = "postgres"
-	dbSQLite    DBType = "sqlite"
-	dbSQLServer DBType = "sqlserver"
-)
-
 type DB = gorm.DB
 
 func MustNew(c *Conf) *DB {
@@ -33,13 +22,13 @@ func MustNew(c *Conf) *DB {
 
 func New(c *Conf) (*DB, error) {
 	var dialector gorm.Dialector
-	if DBType(c.Driver) == dbMySQL {
+	if DBType(c.Driver) == DBType_mysql {
 		dialector = mysql.Open(c.Source)
-	} else if DBType(c.Driver) == dbPostgres {
+	} else if DBType(c.Driver) == DBType_postgres {
 		dialector = postgres.Open(c.Source)
-	} else if DBType(c.Driver) == dbSQLite {
+	} else if DBType(c.Driver) == DBType_sqlite {
 		dialector = sqlite.Open(c.Source)
-	} else if DBType(c.Driver) == dbSQLServer {
+	} else if DBType(c.Driver) == DBType_sqlserver {
 		dialector = sqlserver.Open(c.Source)
 	}
 	gormlogConfig := gormlog.Config{Colorful: true, LogLevel: gormlog.LogLevel(c.LogLevel)}
