beef4be1441905c42365bb6cba5d3fb79f09ab0c
songhao <emberfarkas@hotmail.com>
2023-03-04 14:47:55 +0800

    Update google_storage_test.go

--- fs/google/google_storage_test.go
+++ fs/google/google_storage_test.go
@@ -1,8 +1,6 @@
 package google
 
-import "testing"
-
-func TestUploadImage(t *testing.T) {
-	c := New(&Conf{})
-	c.SaveWebSiteImage("https://img2.baidu.com/it/u=2997444546,2645785836&fm=15&fmt=auto&gp=0.jpg")
-}
+//func TestUploadImage(t *testing.T) {
+//	c := New(&Conf{})
+//	c.SaveWebSiteImage("https://img2.baidu.com/it/u=2997444546,2645785836&fm=15&fmt=auto&gp=0.jpg")
+//}
