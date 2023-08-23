3b3d187c9cbd4b02b003011c0c9cb06fca9d12ac
emberfarkas <emberfarkas@hotmail.com>
2023-08-23 10:49:31 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"flag"
 	"github.com/go-kratos/kratos/v2/config"
 	"net/url"
 	"os"
@@ -9,21 +8,20 @@ import (
 
 type Value = config.Value
 
-var (
-	// conf is the config remote addr
-	conf string
-)
-
-func init() {
-	flag.StringVar(&conf, "conf", "file:///../../configs/conf.yaml", "url for config eg: file:///../../configs/conf.yaml")
+func Load(conf string, v interface{}) config.Config {
+	uri, err := url.Parse(conf)
+	if err != nil {
+		panic(err)
+	}
+	c, err := Create(uri, v)
+	if err != nil {
+		panic(err)
+	}
+	return c
 }
 
-func Load(v interface{}) config.Config {
-	flag.Parse()
-	nacosAddr := os.Getenv("NACOS_ADDRESS")
-	if len(nacosAddr) > 0 {
-		conf = nacosAddr
-	}
+func LoadEnv(v interface{}) config.Config {
+	conf := os.Getenv("NACOS_ADDRESS")
 	uri, err := url.Parse(conf)
 	if err != nil {
 		panic(err)
