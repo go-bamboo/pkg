ea57fe6d909768bccd86aa13e3e8e99f97b6115d
emberfarkas <emberfarkas@hotmail.com>
2023-10-05 18:59:07 +0800

    Update kratos.go

--- kratos/kratos.go
+++ kratos/kratos.go
@@ -3,3 +3,27 @@ package kratos
 import "github.com/go-kratos/kratos/v2"
 
 type App = kratos.App
+
+var New = kratos.New
+
+var ID = kratos.ID
+
+var Name = kratos.Name
+
+var Version = kratos.Version
+
+var Metadata = kratos.Metadata
+
+var Endpoint = kratos.Endpoint
+
+var Context = kratos.Context
+
+var Logger = kratos.Logger
+
+var Server = kratos.Server
+
+var Signal = kratos.Signal
+
+var Registrar = kratos.Registrar
+
+var RegistrarTimeout = kratos.RegistrarTimeout
