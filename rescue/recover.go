0639a84c9e27d3c33545ff4eabff7caa3498af6c
emberfarkas <emberfarkas@hotmail.com>
2024-05-28 10:55:42 +0800

    feat: safe ctx

--- rescue/recover.go
+++ rescue/recover.go
@@ -1,12 +1,26 @@
 package rescue
 
-import "github.com/go-bamboo/pkg/log"
+import (
+	"context"
+	"github.com/go-bamboo/pkg/log"
+)
 
 // Recover is used with defer to do cleanup on panics.
 // Use it like:
 //
 //	defer Recover(func() {})
 func Recover(cleanups ...func()) {
+	for _, cleanup := range cleanups {
+		cleanup()
+	}
+
+	if p := recover(); p != nil {
+		log.ErrorPanic(p)
+	}
+}
+
+// RecoverCtx is used with defer to do cleanup on panics.
+func RecoverCtx(ctx context.Context, cleanups ...func()) {
 	for _, cleanup := range cleanups {
 		cleanup()
 	}
