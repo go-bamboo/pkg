43028ee46eecb1e358ea0e5d379054684586692a
emberfarkas <emberfarkas@hotmail.com>
2024-09-15 13:11:43 +0800

    Update duration.go

--- time/duration.go
+++ time/duration.go
@@ -1,11 +1,20 @@
 package time
 
 import (
+	"encoding"
 	xtime "time"
 )
 
+var _ encoding.TextUnmarshaler = (*Duration)(nil)
+var _ encoding.TextMarshaler = (*Duration)(nil)
+
 // Duration be used toml unmarshal string time, like 1s, 500ms.
 type Duration xtime.Duration
+
+func (d *Duration) MarshalText() (text []byte, err error) {
+	dd := xtime.Duration(*d)
+	return []byte(dd.String()), nil
+}
 
 // UnmarshalText unmarshal text to duration.
 func (d *Duration) UnmarshalText(text []byte) error {
